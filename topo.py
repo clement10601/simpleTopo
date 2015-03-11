4b4282e6dbed39ecc4c6ab009e41c835af905550
clement10601 <clement10601@gmail.com>
2015-03-12 00:35:32 +0800

    new line

--- topo.py
+++ topo.py
@@ -1,51 +1,49 @@
 #!/usr/bin/python
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import OVSSwitch,RemoteController
+from mininet.node import RemoteController
 from mininet.util import dumpNodeConnections
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 
 formatter = "%r"
 block = "####################"
-#c0 = RemoteController( 'c0', ip='127.0.0.1',port=6633 )
+c0 = RemoteController('c0', ip='127.0.0.1', port=6633)
 class dualSH(Topo):
-    def __init__(self,n=5,s=2,**opts):
-        Topo.__init__(self,**opts)
+    def __init__(self, n=2, s=2, **opts):
+        Topo.__init__(self, **opts)
         for sn in range(s):
             info('*** Adding Switch %s\n'%(sn+1))
-            switch = self.addSwitch('s%s'%(sn+1))
-        
+            switch = self.addSwitch('s%s'%(sn+1),
+                                    controller=c0,
+                                    protocols='OpenFlow13')
+
         for hn in range(n):
             info('*** Adding Host %s\n'%(hn+1))
             host = self.addHost('h%s' % (hn+1))
-	k = 1
-	km = int(n/s)+1
-        for i in range(1,s+1):
-	    for j in range(k,km):
+        k = 1
+        km = int(n/s)+1
+        for i in range(1, s+1):
+            for j in range(k, km):
                 self.addLink('h%s' % (j), 's%s' % (i))
-            k=km
-            km=km+int(n/s)
-            if (n%s)!=0:
-                km+=1
-        
+            k = km
+            km = km+int(n/s)
+            if (n%s) != 0:
+                km += 1
+
         info('Adding Switch Link\n')
-        self.addLink('s1','s2')
+        for a in range(2, s+1):
+            self.addLink('s%s'%a, 's1')
 
 def simTest():
-    topo =  dualSH()
-    net = Mininet(topo=topo,build=False)
-    
-    info('*** Adding Controller\n')
-    ryu_ctl = net.addController('c0',controller=RemoteController,ip='127.0.0.1',port=6633)
-    
+    topo = dualSH()
+    net = Mininet(topo=topo, build=False)
+    ryu_ctl = net.addController('c0',
+                                controller=RemoteController,
+                                ip='127.0.0.1', port=6633)
+
     net.build()
     print "NET BUILD"
-    net.getNodeByName('s1').protocols = 'OpenFlow13'
-    net.getNodeByName('s2').protocols = 'OpenFlow13'
-    
-    net.getNodeByName('s1').start([ryu_ctl])
-    net.getNodeByName('s2').start([ryu_ctl])
     net.start()
     print "NET START"
     print formatter % (block)
@@ -59,9 +57,9 @@ def simTest():
     print formatter % (block)
     print "network CLI"
     print formatter % (block)
-    CLI( net )
+    CLI(net)
     net.stop()
-        
+
 if __name__ == '__main__':
     # Tell mininet to print useful information
     setLogLevel('info')
