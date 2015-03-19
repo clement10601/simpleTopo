65a410ea3c883be0b43c0e14672007dfa92ab13c
clement10601 <clement10601@gmail.com>
2015-03-19 18:51:15 +0800

    ilittle change

--- ryu_apps/switch_1.py
+++ ryu_apps/switch_1.py
@@ -1,6 +1,6 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet,ethernet
@@ -8,46 +8,50 @@ import pprint
 
 class L2Switch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
-
     def __init__(self, *args, **kwargs):
         super(L2Switch,self).__init__(*args, **kwargs)
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        self.send_port_desc_stats_request(datapath)
+        print("send_port_desc_stats_request")
+
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self,ev):
+        pass
+
+    def send_port_desc_stats_request(self, datapath):
+        ofp_parser = datapath.ofproto_parser
+        req = ofp_parser.OFPPortDescStatsRequest(datapath, 0)
+        datapath.send_msg(req)
+        print("OFPPortDescStatsRequest")
+
+    def add_flow(self, datapath, in_port, instructions):
+        ofproto = datapath.ofproto
+        ofp_parser = datapath.ofproto_parser
+        match = ofp_parser.OFPMatch(in_port)
+        flow = ofp_parser.OFPFlowMod(datapath=datapath,
+                                 match=match,
+                                 cookie=0,
+                                 command=ofproto.OFPFC_ADD,
+                                 instructions=instructions)
+        datapath.send_msg(flow)
+
+    @set_ev_cls(ofp_event.EventOFPPortDescStatsReply, MAIN_DISPATCHER)
+    def port_desc_stats_reply_handler(self, ev):
         msg = ev.msg
-        dp = msg.datapath
-        ofp = dp.ofproto
-        ofp_parser = dp.ofproto_parser
-
-#        pkt = packet.Packet(msg.data)
-#        eth = pkt.get_protocols(ethernet.ethernet)[0]
-#        dst = eth.dst
-#        src = eth.src
-
-#        dpid = dp.id
-#       self.mac_to_port.setdefault(dpid, {})
-
-        in_port = msg.match['in_port']
-#       self.mac_to_port[dpid][src] = in_port
-
-#       if dst in self.mac_to_port[dpid]:
-#        out_port = self.mac_to_port[dpid][dst]
-#       else:
-#           out_port = ofproto.OFPP_FLOOD
-        out_port = 1
-        if in_port==out_port:
-            out_port = 2
-        else:
-            out_port = 1
-        actions = [ofp_parser.OFPActionOutput(out_port)]
-
-        data = None
-        if msg.buffer_id == ofp.OFP_NO_BUFFER:
-            data = msg.data
-
-        out = ofp_parser.OFPPacketOut(datapath=dp,
-                                      buffer_id=msg.buffer_id,
-                                      in_port=in_port,
-                                      actions=actions,
-                                      data=data)
-        dp.send_msg(out)
+        datapath = msg.datapath
+        ofp = datapath.ofproto
+        ofp_parser = datapath.ofproto_parser
+        ports = []
+        for p in ev.msg.body:
+            if p.port_no < 100:
+                ports.append("%d" % (p.port_no))
+        action = [datapath.ofproto_parser.OFPActionOutput(int(ports[1]))]
+        instructions = [ofp_parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS,action)]
+        self.add_flow(datapath,int(ports[0]),instructions)
+        action = [datapath.ofproto_parser.OFPActionOutput(int(ports[0]))]
+        instructions = [ofp_parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS,action)]
+        self.add_flow(datapath,int(ports[1]),instructions)
