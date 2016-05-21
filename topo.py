398f540a94a6cdaf60714b08912cfd62af3a0fb3
kchwang <clement10601@gmail.com>
2016-05-21 17:43:42 +0800

    Fix and reconstruction

--- topo.py
+++ topo.py
@@ -8,43 +8,58 @@ from mininet.log import setLogLevel, info
 
 formatter = "%r"
 block = "####################"
-c0 = RemoteController('c0', ip='127.0.0.1', port=6633)
-class dualSH(Topo):
-    def __init__(self, n=2, s=2, **opts):
-        Topo.__init__(self, **opts)
-        for sn in range(s):
-            info('*** Adding Switch %s\n'%(sn+1))
-            switch = self.addSwitch('s%s'%(sn+1),
-                                    controller=c0,
-                                    protocols='OpenFlow13')
-
-        for hn in range(n):
-            info('*** Adding Host %s\n'%(hn+1))
-            host = self.addHost('h%s' % (hn+1))
-        k = 1
-        km = int(n/s)+1
-        for i in range(1, s+1):
-            for j in range(k, km):
-                self.addLink('h%s' % (j), 's%s' % (i))
-            k = km
-            km = km+int(n/s)
-            if (n%s) != 0:
-                km += 1
-
-        info('Adding Switch Link\n')
-        for a in range(2, s+1):
-            self.addLink('s%s'%a, 's1')
+onos1 = "10.0.3.86"
+onos2 = "10.0.3.87"
 
 def simTest():
-    topo = dualSH()
-    net = Mininet(topo=topo, build=False)
-    ryu_ctl = net.addController('c0',
-                                controller=RemoteController,
-                                ip='127.0.0.1', port=6633)
-
+    net = Mininet(topo=None,build=False)
+    ct0 = net.addController('c0',
+    	controller=RemoteController,
+        ip=onos1, port=6653)
+    ct1 = net.addController('c1',
+        controller=RemoteController,
+        ip=onos2, port=6653)
+    ctrl = []
+    ctrl.append(RemoteController('c0', ip = onos1, port=6653))
+    ctrl.append(RemoteController('c1', ip = onos2, port=6653))
+    switches=2
+    hosts=2
+    controllers=2
+    switch = []
+    host = []
+    def addNetFunc(switches,hosts,controllers):
+        for cc in range(controllers):
+            for sn in range(switches):
+                info('*** Adding Switch %s for controller %s\n'%(sn+1,cc+1))
+                switch.append(net.addSwitch('sc%s%s'%(sn+1,cc+1),
+                                         controller=ctrl[cc],
+                                         protocols='OpenFlow13'))
+            for hn in range(hosts):
+                info('*** Adding Host %s\n'%(hn+1))
+                host.append(net.addHost('hc%s%s' % (hn+1,cc+1)))
+            k = 1
+            km = int(hosts/switches)+1
+            for i in range(1, switches+1):
+                for j in range(k, km):
+                    net.addLink('hc%s%s' % (j,cc+1), 'sc%s%s' % (i,cc+1))
+                k = km
+                km = km+int(hosts/switches)
+                if (hosts%switches) != 0:
+                    km += 1
+            info('Adding Switch Link\n')
+            for a in range(1, switches):
+                net.addLink('sc%s%s'%(a,cc+1), 'sc%s%s'%(a+1,cc+1)) 
+    addNetFunc(switches,hosts,controllers)
+    net.addLink('sc11','sc12')
     net.build()
+    net.build()
+    switch[0].start([ct0]) 
+    switch[1].start([ct0]) 
+    switch[2].start([ct1]) 
+    switch[3].start([ct1]) 
     print "NET BUILD"
-    net.start()
+#    net.start()
+#    net.start()
     print "NET START"
     print formatter % (block)
     print "Dumping host connections"
